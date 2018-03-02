28e8b27c7c4906e423d04792d512a012d4e88c10
WorkingCreed <creed@rothstaffing.com>
2018-03-02 14:46:36 -0800

    Adding Open Food Api Project

---
+++ Pantry.OpenFood/Models/OpenFood.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp.Deserializers;
+
+namespace Pantry.OpenFood.Models
+{
+  public class OpenFood
+  {
+    
+    [DeserializeAs(Name = "product")]
+    public Product Product { get; set; }
+    [DeserializeAs(Name = "status_verbose")]
+    public string StatusVerbose { get; set; }
+    [DeserializeAs(Name = "status")]
+    public int Status { get; set; }
+    [DeserializeAs(Name = "code")]
+    public long Code { get; set; }
+  }
+}
---
+++ Pantry.OpenFood/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Pantry.OpenFood.Models
+{
+  public class Product
+  {
+    public Sources Sources { get; set; }
+    public string ProductName { get; set; }
+  }
+}
