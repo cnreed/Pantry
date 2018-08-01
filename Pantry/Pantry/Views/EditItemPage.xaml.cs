b3cf132616941b88b7a688c7b1a4227da7360c66
WorkingCreed <creed@rothstaffing.com>
2018-08-01 14:36:10 -0700

    Beginning work on edit item page

---
+++ Pantry/Pantry/Views/EditItemPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Pantry.Views
+{
+    public partial class EditItemPage : ContentPage
+    {
+        public EditItemPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
